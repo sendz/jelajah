c641314e410a03841833dd95edbe7f7bd2025033
sendz <sendzation@gmail.com>
2015-05-19 20:15:52 +0700

    Add trip

--- application/views/dashboard/addtrip.php
+++ application/views/dashboard/addtrip.php
@@ -43,13 +43,72 @@
       <div class="input-field col s12 m2">
       <?php
         echo form_label('Time','add-detail-etd-time');
-        $add_detail_etd_date = array(
+        $add_detail_etd_time = array(
             'id'      => 'add-detail-etd-time'
           , 'name'    => 'add-detail-etd-time'
           , 'type'    => 'text'
           , 'class'   => 'timepicker'
         );
-        echo form_input($add_detail_etd_date);
+        echo form_input($add_detail_etd_time);
+      ?>
+      </div>
+      <div class="input-field col s12 m4">
+      <?php
+        echo form_label('Date to Arrive','add-detail-eta');
+        $add_detail_eta = array(
+            'id'      => 'add-detail-eta'
+          , 'name'    => 'add-detail-eta'
+          , 'type'    => 'text'
+          , 'class'   => 'datepicker'
+        );
+        echo form_input($add_detail_eta);
+      ?>
+      </div>
+      <div class="input-field col s12 m2">
+      <?php
+        echo form_label('Time','add-detail-eta-time');
+        $add_detail_eta_time = array(
+            'id'      => 'add-detail-eta-time'
+          , 'name'    => 'add-detail-eta-time'
+          , 'type'    => 'text'
+          , 'class'   => 'timepicker'
+        );
+        echo form_input($add_detail_eta_time);
+      ?>
+      </div>
+    </div>
+    <div class="row">
+      <div class="input-field col s12 m6">
+      <?php
+        echo form_label('Carrier (Airline or Transporter Name)','add-detail-carrier');
+        $add_detail_carrier = array(
+            'id'      => 'add-detail-carrier'
+          , 'name'    => 'add-detail-carrier'
+          , 'type'    => 'text'
+        );
+        echo form_input($add_detail_carrier);
+      ?>
+      </div>
+      <div class="input-field col s12 m3">
+      <?php
+        echo form_label('Flight Number *if any','add-detail-flight-number');
+        $add_detail_flight_number = array(
+            'id'      => 'add-detail-flight-number'
+          , 'name'    => 'add-detail-flight-number'
+          , 'type'    => 'text'
+        );
+        echo form_input($add_detail_flight_number);
+      ?>
+      </div>
+      <div class="input-field col s12 m3">
+      <?php
+        echo form_label('Booking Code *if any','add-detail-booking-code');
+        $add_detail_booking_code = array(
+            'id'      => 'add-detail-booking-code'
+          , 'name'    => 'add-detail-booking-code'
+          , 'type'    => 'text'
+        );
+        echo form_input($add_detail_booking_code);
       ?>
       </div>
     </div>
